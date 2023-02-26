3f292408cc191324047cd360a77d6e8928c70e2e
Berke Tinas <ember.shared@gmail.com>
2023-02-26 16:21:19 +0300

    init prettier

--- src/components/gallery-item/gallery-item.tsx
+++ src/components/gallery-item/gallery-item.tsx
@@ -6,15 +6,16 @@ type PropTypes = {
 
 export const GalleryItem = ({ name, description, children }: PropTypes) => (
     <>
-
-    <div className='flex flex-col items-center gap-y-8'>
-        <div className='text-center'>
-        <p className='m-0 px-2 py-1 text-black text-2xl font-bold'>{name}</p>
-        <div className='m-0 px-2 py-1 text-gray text-base font-light'>
-            {description}
+        <div className='flex flex-col items-center gap-y-8'>
+            <div className='text-center'>
+                <p className='m-0 px-2 py-1 text-black text-2xl font-bold'>
+                    {name}
+                </p>
+                <div className='m-0 px-2 py-1 text-gray text-base font-light'>
+                    {description}
+                </div>
+            </div>
+            <div className='flex justify-center w-full gap-x-4'>{children}</div>
         </div>
-    </div>
-        <div className='flex justify-center w-full gap-x-4'>{children}</div>
-    </div>
     </>
 );
--- src/pages/component-gallery.tsx
+++ src/pages/component-gallery.tsx
@@ -3,19 +3,13 @@ import { GalleryItem } from '@/components/gallery-item/gallery-item';
 export default function ComponentGallery() {
     return (
         <div className='flex flex-col py-8 gap-y-16'>
-            <GalleryItem
-                name='test name'
-                description='test desc'
-            >
-                    <p>first gallery item</p>
+            <GalleryItem name='test name' description='test desc'>
+                <p>first gallery item</p>
             </GalleryItem>
             <hr />
-            <GalleryItem
-                name='test name 2'
-                description='test desc 2'
-                >
-                    <p>second gallery item</p>
-                </GalleryItem>
+            <GalleryItem name='test name 2' description='test desc 2'>
+                <p>second gallery item</p>
+            </GalleryItem>
         </div>
     );
 }
